3cf23dc53ef78420b6354222deaf96b9b877364b
Adam Yao <adamyao3240@gmail.com>
2023-11-30 23:35:54 -0500

    password chagne

--- friend-db.php
+++ friend-db.php
@@ -24,10 +24,10 @@ function addFriend($friendname, $major, $year)
 function registerApplicant($username, $password, $email)
 {
   global $db;
-  $query = "insert into Applicant(username, passwrd, email) values (:username, :passwrd, :email) ";
+  $query = "insert into Applicant(username, password, email) values (:username, :password, :email) ";
   $statement = $db->prepare($query);
   $statement->bindValue(':username', $username);
-  $statement->bindValue(':passwrd', $password);
+  $statement->bindValue(':password', $password);
   $statement->bindValue(':email', $email);
   $statement->execute();
   $statement->closeCursor();
